9b292985109e102b43cf77202b6c87bcc9b8a414
Kalvin Tipton <86502093+Shikamaru5@users.noreply.github.com>
2022-01-30 14:55:43 -0700

    Create Omnitrix.py
    Gives you a set of 10 random aliens, I tend to use my imagination a lot and sometimes find it's nice to be able to get some random unbiased elements to the games, which for instance might be something like this where you start out the adventure with 10 random aliens just like in the shows. This just makes it easier so that you don't have to go to bizarre extreme lengths to get a random selection.

---
+++ Omnitrix.py
@@ -0,0 +1,13 @@
+    import random
+
+    for i in range(10):
+	 Omnitrix = ['upgrade', 'four-arms', 'ripjaws', 'greymatter', 'rath',
+	 'heatblast', 'diamondhead', 'ghostfreak', 'wildmutt', 'stinkfly',
+	 'cannonbolt', 'wildvine', 'waybig', 'ditto', 'eyeguy', 'xlr8',
+	 'benwolf', 'benmummy', 'benvicktor', 'swampfire', 'jetray', 'goop',
+	 'humungasaur', 'spidermonkey', 'benvilgax', 'chromastone', 'brainstorm',
+	 'articguanna', 'spitter', 'nanomech', 'buzzshock', 'benlevin',
+	 'big-chill', 'echo-echo', 'upchuck', 'ampfibian', 'nrg', 'armodrillo',
+	 'chamalien', 'clockwork', 'water hazard', 'fasttrack', 'terraspin']
+
+    print(random.choice(Omnitrix))
